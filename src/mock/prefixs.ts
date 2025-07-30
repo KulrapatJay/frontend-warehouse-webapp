b3472f435be141cec0047f3b3fb8c7d081b3504c
punnathat34 <151804801+punnathat34@users.noreply.github.com>
2025-07-31 00:42:41 +0700

    เพิ่ม role and prefix เเละเเก้ไข user ให้ตรงตามdatabase

---
+++ src/mock/prefixs.ts
@@ -0,0 +1,10 @@
+export const Prefix = [
+    {
+        id: 1001,
+        name: 'Mr.',
+    },
+    {
+        id: 1002,
+        name: 'Ms.',
+    }
+];
---
+++ src/mock/roles.ts
@@ -0,0 +1,18 @@
+export const role = [
+    {
+        id: 1,
+        role_name: 'user'
+    },
+    {
+        id: 2,
+        role_name: 'manager'
+    },
+    {
+        id: 3,
+        role_name: 'admin'
+    },
+    {
+        id: 4,
+        role_name: 'staff'
+    }
+];
